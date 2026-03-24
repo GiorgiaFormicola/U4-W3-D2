7cccbbbb0ae072e13c361f7261f5a304a2048e39
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-03-24 14:25:50 +0100

    DB connected

--- src/main/java/GiorgiaFormicola/Application.java
+++ src/main/java/GiorgiaFormicola/Application.java
@@ -1,8 +1,16 @@
 package GiorgiaFormicola;
 
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
+    private static final EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("events_management_pu");
 
     public static void main(String[] args) {
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
         System.out.println("Hello World!");
+        entityManager.close();
+        entityManagerFactory.close();
     }
 }
