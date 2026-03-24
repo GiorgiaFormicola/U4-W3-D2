b6cc0031a8708680a6f9f008691e51601cf5ea2d
GiorgiaFormicola <giorgia.formicola97@gmail.com>
2026-03-24 16:03:42 +0100

    Adds getById method to EventsDAO

--- src/main/java/GiorgiaFormicola/Application.java
+++ src/main/java/GiorgiaFormicola/Application.java
@@ -3,6 +3,7 @@ package GiorgiaFormicola;
 import GiorgiaFormicola.dao.EventsDAO;
 import GiorgiaFormicola.entities.Event;
 import GiorgiaFormicola.enums.EventType;
+import GiorgiaFormicola.exceptions.NotFoundException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
@@ -18,15 +19,29 @@ public class Application {
         Event event2 = new Event("Louis C.K. Show", 2025, 3, 1, "Hilarious stand-up comedy", EventType.PUBLIC, 1500);
         Event event3 = new Event("Architecture Conference", 2027, 1, 12, "Conference on the latest trends in architecture field", EventType.PRIVATE, 300);
 
-        System.out.println(event1);
+       /* System.out.println(event1);
         System.out.println(event2);
-        System.out.println(event3);
+        System.out.println(event3);*/
 
-        try {
+        /*try {
             eventsDAO.save(event1);
             eventsDAO.save(event2);
             eventsDAO.save(event3);
         } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }*/
+
+        try {
+            Event event1FromDB = eventsDAO.getById(1);
+            System.out.println(event1FromDB);
+        } catch (NotFoundException e) {
+            System.out.println(e.getMessage());
+        }
+
+        try {
+            Event event4FromDB = eventsDAO.getById(4);
+            System.out.println(event4FromDB);
+        } catch (NotFoundException e) {
             System.out.println(e.getMessage());
         }
 
--- src/main/java/GiorgiaFormicola/dao/EventsDAO.java
+++ src/main/java/GiorgiaFormicola/dao/EventsDAO.java
@@ -1,6 +1,7 @@
 package GiorgiaFormicola.dao;
 
 import GiorgiaFormicola.entities.Event;
+import GiorgiaFormicola.exceptions.NotFoundException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityTransaction;
 
@@ -17,5 +18,11 @@ public class EventsDAO {
         entityManager.persist(newEvent);
         transaction.commit();
         System.out.println("The event " + newEvent.getTitle() + " has been saved successfully");
+    }
+
+    public Event getById(long id) {
+        Event eventFound = entityManager.find(Event.class, id);
+        if (eventFound == null) throw new NotFoundException(id);
+        return eventFound;
     }
 }
